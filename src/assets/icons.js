604c1d99b05bd5819a00caf6f5c05f70d7c3e693
CayaCoco <melcayacoco@gmail.com>
2022-07-09 03:39:05 -0400

    icons

---
+++ src/assets/icons.js
@@ -0,0 +1,21 @@
+import PermContactCalendarIcon from '@mui/icons-material/PermContactCalendar';
+import MessageIcon from '@mui/icons-material/Message';
+import CorporateFareIcon from '@mui/icons-material/CorporateFare';
+import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import HomeIcon from '@mui/icons-material/Home';
+
+
+export const Icons = () => {
+
+    return (
+        <>
+            <PermContactCalendarIcon />
+            <MessageIcon />
+            <CorporateFareIcon />
+            <InsertDriveFileIcon />
+            <CalendarMonthIcon />
+            <HomeIcon />
+        </>
+    )
+};

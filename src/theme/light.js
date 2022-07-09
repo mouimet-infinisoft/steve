204df53fefe79c4e8a52ae67d9bc77a6e552a6fa
CayaCoco <melcayacoco@gmail.com>
2022-07-09 03:13:01 -0400

    theme3

--- src/theme/light.js
+++ src/theme/light.js
@@ -1,8 +1,8 @@
-import { ThemeOptions } from '@material-ui/core/styles/createMuiTheme';
+import { createTheme } from "@mui/material/styles";
 
-export const themeOptions = ThemeOptions({
+export const lightTheme = createTheme({
   palette: {
-    type: 'light',
+    mode: 'light',
     primary: {
       main: '#3f51b5',
     },
@@ -12,107 +12,5 @@ export const themeOptions = ThemeOptions({
   },
   typography: {
     fontFamily: 'Orbitron',
-  },
-  props: {
-    MuiTooltip: {
-      arrow: true,
-    },
-    MuiList: {
-      dense: true,
-    },
-    MuiMenuItem: {
-      dense: true,
-    },
-    MuiTable: {
-      size: 'small',
-    },
-    MuiButton: {
-      size: 'small',
-    },
-    MuiButtonGroup: {
-      size: 'small',
-    },
-    MuiCheckbox: {
-      size: 'small',
-    },
-    MuiFab: {
-      size: 'small',
-    },
-    MuiFormControl: {
-      margin: 'dense',
-      size: 'small',
-    },
-    MuiFormHelperText: {
-      margin: 'dense',
-    },
-    MuiIconButton: {
-      size: 'small',
-    },
-    MuiInputBase: {
-      margin: 'dense',
-    },
-    MuiInputLabel: {
-      margin: 'dense',
-    },
-    MuiRadio: {
-      size: 'small',
-    },
-    MuiSwitch: {
-      size: 'small',
-    },
-    MuiTextField: {
-      margin: 'dense',
-      size: 'small',
-    },
-    MuiButtonBase: {
-      disableRipple: true,
-    },
-  },
-  overrides: {
-    MuiSwitch: {
-      root: {
-        width: 42,
-        height: 26,
-        padding: 0,
-        margin: 8,
-      },
-      switchBase: {
-        padding: 1,
-        '&$checked, &$colorPrimary$checked, &$colorSecondary$checked': {
-          transform: 'translateX(16px)',
-          color: '#fff',
-          '& + $track': {
-            opacity: 1,
-            border: 'none',
-          },
-        },
-      },
-      thumb: {
-        width: 24,
-        height: 24,
-      },
-      track: {
-        borderRadius: 13,
-        border: '1px solid #bdbdbd',
-        backgroundColor: '#fafafa',
-        opacity: 1,
-        transition: 'background-color 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,border 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
-      },
-    },
-    MuiButton: {
-      root: {
-        background: 'linear-gradient(45deg, #3f51b5 30%, #f50057 90%)',
-        border: 0,
-        borderRadius: 3,
-        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-        color: 'white',
-        height: 48,
-        padding: '0 30px',
-      },
-    },
-  },
-  shape: {
-    borderRadius: 4,
-  },
-  spacing: 8,
-});
+  }
+  });

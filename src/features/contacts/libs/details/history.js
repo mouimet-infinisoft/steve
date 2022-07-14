0bbacd15fdc4973075e5ff915373367bb34081fa
Jalal khan <jaalk47@gmail.com>
2022-07-14 21:02:55 +0100

    User history mock completed

--- src/features/contacts/libs/details/history.js
+++ src/features/contacts/libs/details/history.js
@@ -6,56 +6,56 @@ import TimelineConnector from '@mui/lab/TimelineConnector';
 import TimelineContent from '@mui/lab/TimelineContent';
 import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
 import TimelineDot from '@mui/lab/TimelineDot';
-import FastfoodIcon from '@mui/icons-material/Fastfood';
 import Typography from '@mui/material/Typography';
 import { Box } from '@mui/material';
 import { useTheme } from '@mui/system';
 import { useMicroState } from "@/core/state";
 import {config} from '../../config'
 import { featurehistory } from '../../mock/featurelist';
+import CreateNewFolderIcon from '@mui/icons-material/CreateNewFolder';
+import UpdateIcon from '@mui/icons-material/Update';
+import DeleteIcon from '@mui/icons-material/Delete';
 
-
-const getHistoryIcon = (action) => {
-  switch (true) {
-    case action==="create":
-      return <FastfoodIcon />;
-    case action==="create":
-      return <FastfoodIcon />;
-    case action==="create":
-      return <FastfoodIcon />;
-}}
+const getHistoryIcon =  {
+  creation: <CreateNewFolderIcon color="success"/>,
+  update: <UpdateIcon  color="primary"/>,
+  delete: <DeleteIcon  color="error"/>,
+}
 
 const HistoryTimeLine = ()=> {
   const theme = useTheme();
   const selectedId = useMicroState((s) => s[config.feature.name].selectedId);
-  const userHistory = featurehistory().find((item) => item[selectedId]);
-  const selectedUserHistory = userHistory[selectedId]
-  const userMeta = selectedUserHistory.__meta__
+  const historyList = featurehistory().filter((item) => item[selectedId]?.id === selectedId);
   return (
     <Box height={theme.spacing(32)} overflow="auto">
-    <Timeline position="alternate">
-      <TimelineItem>
+    <Timeline>
+      {historyList.map((history) => {
+        const userHistory = Object.values(history)[0];
+        return (
+        <TimelineItem>
         <TimelineOppositeContent
           sx={{ m: 'auto 0' }}
           variant="body2"
           color="text.secondary"
         >
-          {new Date(userMeta.timestamp).toDateString()}
+          {new Date(userHistory.__meta__.timestamp).toDateString()}
         </TimelineOppositeContent>
         <TimelineSeparator>
           <TimelineConnector />
           <TimelineDot>
-          {getHistoryIcon(userMeta.action)}
+           {getHistoryIcon[userHistory.__meta__.action]}
           </TimelineDot>
           <TimelineConnector />
         </TimelineSeparator>
         <TimelineContent>
           <Typography variant="body2">
-          {selectedUserHistory.email}
+          {userHistory.email}
           </Typography>
-          <Typography variant="body2" component="span" >{selectedUserHistory.name} Because you need strength</Typography>
+          <Typography variant="body2" component="span" >{userHistory.name} </Typography>
         </TimelineContent>
-      </TimelineItem>
+        </TimelineItem>
+        )
+})}
     </Timeline>
     </Box>
   );
--- src/features/contacts/mock/featurelist.js
+++ src/features/contacts/mock/featurelist.js
@@ -33,6 +33,93 @@ export const featurehistory = () => [
     }
   },
   {
+    klhlojassasssaso897y9: {
+      id: "klhlojassasssaso897y9",
+      name: "Martin Ouimet",
+      avatar: cat1,
+      address: "3649 rue Notre-Dame, Candiax, H72D92",
+      telephones: [
+        { key: 111976, label: "514-777-5555", tag: "Work" },
+        { key: 567222, label: "540-789-2121", tag: "Mobile" }
+      ],
+      email: "dog@pound.net",
+      tags: ["person"],
+      state: "active",
+      __meta__: {
+        action: 'update',
+        timestamp: Date.now(),
+        createdAt: Date.now(),
+        updatedAt: Date.now(),
+        username: "Jean Dupuis",
+        email: "jena@dupuis.com",
+        avatar: "https://i.pravatar.cc/48?" + Math.random()
+      },
+      __relation__: [
+        { id: "121313424", key: "employer", value: "pfizer" },
+        { id: "fdas54ff54", key: "father", value: "case32423" }
+      ],
+      __extra__: [{ id: "@212131", key: "Allergy", value: "Peannuts" }]
+    }
+  },
+  {
+    klhlojassasssaso897y9: {
+      id: "klhlojassasssaso897y9",
+      name: "Martin Ouimet",
+      avatar: cat1,
+      address: "3649 rue Notre-Dame, Candiax, H72D92",
+      telephones: [
+        { key: 111976, label: "514-777-5555", tag: "Work" },
+        { key: 567222, label: "540-789-2121", tag: "Mobile" }
+      ],
+      email: "dog@pound.net",
+      tags: ["person"],
+      state: "active",
+      __meta__: {
+        action: 'delete',
+        timestamp: Date.now(),
+        createdAt: Date.now(),
+        updatedAt: Date.now(),
+        username: "Jean Dupuis",
+        email: "jena@dupuis.com",
+        avatar: "https://i.pravatar.cc/48?" + Math.random()
+      },
+      __relation__: [
+        { id: "121313424", key: "employer", value: "pfizer" },
+        { id: "fdas54ff54", key: "father", value: "case32423" }
+      ],
+      __extra__: [{ id: "@212131", key: "Allergy", value: "Peannuts" }]
+    }
+  },
+  {
+    klhlojassasssaso897y9: {
+      id: "klhlojassasssaso897y9",
+      name: "Martin Ouimet",
+      avatar: cat1,
+      address: "3649 rue Notre-Dame, Candiax, H72D92",
+      telephones: [
+        { key: 111976, label: "514-777-5555", tag: "Work" },
+        { key: 567222, label: "540-789-2121", tag: "Mobile" }
+      ],
+      email: "dog@pound.net",
+      tags: ["person"],
+      state: "active",
+      __meta__: {
+        action: 'update',
+        timestamp: Date.now(),
+        createdAt: Date.now(),
+        updatedAt: Date.now(),
+        username: "Jean Dupuis",
+        email: "jena@dupuis.com",
+        avatar: "https://i.pravatar.cc/48?" + Math.random()
+      },
+      __relation__: [
+        { id: "121313424", key: "employer", value: "pfizer" },
+        { id: "fdas54ff54", key: "father", value: "case32423" }
+      ],
+      __extra__: [{ id: "@212131", key: "Allergy", value: "Peannuts" }]
+    }
+  },
+  {
     kngfhnhfgngfhnfghngfhn9: {
       id: "kngfhnhfgngfhnfghngfhn9",
       telephones: [

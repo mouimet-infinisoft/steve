9848eb14653d45fd338fa5007ac2f8b3775a5b2a
Jalal khan <jaalk47@gmail.com>
2022-07-19 00:12:00 +0100

    minor change

--- src/features/contacts/libs/details/history.js
+++ src/features/contacts/libs/details/history.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Box } from '@mui/material';
+import Box from '@mui/material/Box';
 import { useTheme } from '@mui/system';
 import { useMicroState } from "@/core/state";
 import { config } from '../../config'

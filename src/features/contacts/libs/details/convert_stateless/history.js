dd6dbf29570091ba75e94ea933d9551153aed28e
Jalal khan <jaalk47@gmail.com>
2022-07-21 00:18:50 +0100

    refactoring changes

--- src/features/contacts/libs/details/convert_stateless/history.js
+++ src/features/contacts/libs/details/convert_stateless/history.js
@@ -1,24 +1,14 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import { useTheme } from '@mui/system';
-import { useMicroState } from "@/core/state";
-import { config } from '../../../config'
-import { featurehistory } from '../../../mock/featurelist';
 import HistoryTimeLine from './history.timeline';
 import HistoryDetails from './history.detail';
 import ScrollContainer from '@/components/scroll-container';
-import { useItem } from '@/core/hooks';
 import SlideEffect from '@/components/slide-effect';
 
-const History = () => {
+const HistoryTimelineAndDetails = ({ selectedHistory, historyList, setShowHistoryDetails, showHistoryDetails }) => {
   const theme = useTheme();
-  const selectedId = useMicroState((s) => s[config.feature.name].selectedId);
-  const { item } = useItem({
-    id: selectedId,
-    feature: config.feature.name
-  });
-  const historyList = featurehistory().filter((item) => item[selectedId]?.id === selectedId);
-  const [showHistoryDetails, setShowHistoryDetails] = React.useState(null);
+
   return (
     <Box height={theme.spacing(32)} sx={{ overflow: "auto", overflowX: "hidden" }}>
       {!showHistoryDetails && (
@@ -28,12 +18,12 @@ const History = () => {
       )}
       {showHistoryDetails && (
         <SlideEffect direction='right' styleProps={{ textAlign: "start" }}>
-          <ScrollContainer name={item.name} email={item.email} date={showHistoryDetails.__meta__.timestamp} action={showHistoryDetails.__meta__.action} goBack={() => setShowHistoryDetails(false)}>
-            <HistoryDetails showHistoryDetails={showHistoryDetails.history} goBack={() => setShowHistoryDetails(false)} showNoRecordFound={Object.values(showHistoryDetails).length} />
+          <ScrollContainer name={selectedHistory?.name} email={selectedHistory?.email} date={showHistoryDetails?.__meta__?.timestamp} action={showHistoryDetails?.__meta__?.action} goBack={() => setShowHistoryDetails(false)}>
+            <HistoryDetails showHistoryDetails={showHistoryDetails?.history} goBack={() => setShowHistoryDetails(false)} showNoRecordFound={Object.values(showHistoryDetails).length} />
           </ScrollContainer>
         </SlideEffect>
       )}
     </Box>
   );
 }
-export default History;
+export default HistoryTimelineAndDetails;

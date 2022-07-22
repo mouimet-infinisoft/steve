fbdce94a5d5067f000acee940709bca8742bc65b
Martin Ouimet <mouimet@infini-soft.com>
2022-07-21 20:52:23 -0400

    close

--- src/components/tabs/basic/ObjectTabs.js
+++ src/components/tabs/basic/ObjectTabs.js
@@ -1,4 +1,4 @@
 import * as React from "react";
 import { ArrayTabs } from "./ArrayTabs";
 
-export const ObjectTabs = ({ tabs }) => <ArrayTabs tabs={Object.entries(tabs)} />;
+export const ObjectTabs = ({ tabs }) => <ArrayTabs tabs={Object.entries(tabs).map(([k, val])=>({label: k, ...val}))} />;

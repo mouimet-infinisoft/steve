8430f046c8131f167af664a9c3ec3e93c465dbdd
Martin Ouimet <mouimet@infini-soft.com>
2022-07-21 20:37:39 -0400

    linktabs variant

--- src/components/tabs/basic/ArrayTabs.js
+++ src/components/tabs/basic/ArrayTabs.js
@@ -2,20 +2,15 @@ import * as React from "react";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Box from "@mui/material/Box";
-import { Link } from "react-router-dom";
 import { a11yProps } from ".";
 
-export const ArrayTabs = ({ tabs }) => {
+export const ArrayTabs = ({ tabs, TabSlot=Tab }) => {
   const [value, setValue] = React.useState(Object.keys(tabs)?.[0]);
 
   const handleChange = (event, newValue) => {
     React.startTransition(() => setValue(newValue));
   };
-  const LinkTab = ({ to = "/", ...tab }) => (
-    <Link to={to}>
-      <Tab {...tab} />
-    </Link>
-  );
+
   return (
     <Box sx={{ width: "100%" }}>
       <Box
@@ -23,10 +18,10 @@ export const ArrayTabs = ({ tabs }) => {
       >
         <Tabs value={value} onChange={handleChange} aria-label="tabs">
           {tabs?.map(({ component, label, ...tab }, i) => (
-            <LinkTab
+            <TabSlot
               key={`${tab?.label}-${i}`}
+              value={label ?? i}
               {...tab}
-              value={label}
               {...a11yProps(i)} />
           ))}
         </Tabs>
--- src/components/tabs/link-tabs.js
+++ src/components/tabs/link-tabs.js
@@ -1,35 +1,16 @@
 import * as React from "react";
-import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
-import Box from "@mui/material/Box";
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
+import BasicTabs from "./basic";
 
-function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    "aria-controls": `simple-tabpanel-${index}`
-  };
-}
-const LinkTab = ({to='/', ...tab}) =>  <Link to={to}><Tab {...tab} /></Link>
+const LinkTab = ({ to = "/", ...tab }) => (
+  <Link to={to}>
+    <Tab {...tab} />
+  </Link>
+);
 
+const LinkTabs = ({ tabs, props }) => (
+  <BasicTabs tabs={tabs} TabSlot={LinkTab} {...props} />
+);
 
-export default function LinkTabs({ tabs=[] }) {
-  const [value, setValue] = React.useState(0);
-  const {pathname="/"} = useLocation()
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
-  return (
-    <Box sx={{ width: "100%", marginBottom: "2rem" }}>
-      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-        <Tabs value={value} onChange={handleChange} aria-label="tabs">
-          {tabs?.map(({to, ...tab}, i) => (
-            <LinkTab key={`${tab?.label}-${i}`} to={`${pathname}/${to}`} {...tab} {...a11yProps(i)} />
-          ))}
-        </Tabs>
-      </Box>
-    </Box>
-  );
-}
+export default LinkTabs;
